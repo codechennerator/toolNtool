06d8e89167c58b4b3463d27770462176a9b70deb
Nathan Chen <chenl.nathan@gmail.com>
2018-03-20 16:11:46 -0700

    making keys work on production

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 //!!CONFIGURE THESE VARIABLES IN PRODUCTION.
 
 module.exports = {
-    "googleClientID": "1080879829742-bk875vi0hs7tifmodmsu6rn5c1jj4u8o.apps.googleusercontent.com",
-    "googleClientSecret": "gPEyMFmNQjmssKHxUjaDkpu1",
-    "mongoURI": "mongodb://localhost:27017/testUserDB",
-    "cookieKey": "oelsifjewoifjwoifjaiofjeaiofjaewoifOIJFEOWIFJEOIF231552512qfoijf"
+    "googleClientID": process.env.GOOGLE_CLIENT_ID,
+    "googleClientSecret": process.env.GOOGLE_CLIENT_SECRET,
+    "mongoURI": process.env.MONGO_URI,
+    "cookieKey": process.env.COOKIE_KEY
 };
