45aaa28ea3dbc8c72ff0b8ad2606d9ba19e74fcb
Nathan Chen <chenl.nathan@gmail.com>
2018-03-24 10:53:48 -0700

    redid nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -61,15 +61,13 @@ class Nav extends Component {
       }} 
       >
         <Menu pointing secondary >
-          {/* <Menu.Item>
+          <Menu.Item>
             <a href="/"><img src={t2}  style={logoStyle}/></a>
-          </Menu.Item> */}
+          </Menu.Item>
           <Menu.Item name='Tool N Tool' href="/" style={fontStyle}/>
           <Menu.Item name='find All' href="/findAll" style={fontStyle}/>
           <Menu.Item name='post Tool' href="/postTool" style={fontStyle}/>
           {this.renderContent()}
-          {/* <Menu.Item name='Login' href="/auth/google" style={fontStyle} />
-          <Menu.Item name='Logout' href='/auth/logout' style={fontStyle} /> */}
           {window.location.href.slice(-1) !== "/" && 
           <div style={inputStyle}>
           
