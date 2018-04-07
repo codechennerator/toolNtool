9966d5c88c122c6a0922522db62e00e152c1e6fc
Nathan Chen <chenl.nathan@gmail.com>
2018-04-06 22:05:33 -0700

    fragments of zhong testmonials

--- client/src/pages/startPage/startPage.js
+++ client/src/pages/startPage/startPage.js
@@ -3,7 +3,7 @@ import SearchForm from "../../components/Search";
 import { Container } from "semantic-ui-react";
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-
+// import Testmonials from "../../components/Testmonials/Testmonials"
 // const HomepageHeading = ({ mobile }) => (
 
 // );
@@ -13,6 +13,7 @@ class startPage extends Component{
     return (
       <div>
       <SearchForm location = {this.props.location}/>
+      {/* <Testmonials /> */}
       </div>
     );
   }
