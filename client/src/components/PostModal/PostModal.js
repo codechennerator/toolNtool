84d91992aedd90741a39396f2d7569e4f511e759
mtoasis <mtoasis22@gmail.com>
2018-04-25 19:13:02 -0700

    post modal update
    alert feature added to post modal when fields are not fulfileld

--- client/src/components/PostModal/PostModal.js
+++ client/src/components/PostModal/PostModal.js
@@ -6,19 +6,17 @@ import axios from 'axios'
 
 
 class PostModal extends Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
         this.state = {
-          fireRedirect: false,
-          image:null,
-          isSubmitted: false
+            fireRedirect: false,
+            image: null,
+            isFulfilled: false
         }
-      }
+    }
 
-    handleFormSubmit(){
-
-          
-          API.savePost({
+    handleFormSubmit() {
+        API.savePost({
             title: this.props.info.title,
             description: this.props.info.description,
             user: this.props.user._id,
@@ -26,63 +24,80 @@ class PostModal extends Component {
             location: this.props.info.location,
             coordinate: this.props.info.coordinate,
             price: this.props.info.price
-          })
+        })
             .then(res => {
                 this.setState({ fireRedirect: true })
             })
-            .catch(err => console.log(err));
-        
+            .catch(err => console.log(err))
+
     };
 
-    submitImage() {
+    componentDidMount() {
         if (
-            this.props.info.title !=="" && 
-            this.props.info.description !=="" &&
-            this.props.info.imageUrl !==null &&
-            this.props.info.price !==null &&
-            this.props.info.location !==""){
-            this.setState({isSubmitted: true})
-            axios.post('https://api.cloudinary.com/v1_1/daretodate/image/upload',{
-            "file":this.props.info.imagePreviewUrl,
-            "upload_preset": "v4gae7vn",    
-            },
-            { "X-Requested-With": "XMLHttpRequest" })
-            .then(response=>{
-            this.setState({image:response.data.secure_url})
-            console.log(this.state.image)
-
-            this.handleFormSubmit()
-            })
+            this.props.info.title !== "" &&
+            this.props.info.description !== "" &&
+            this.props.info.imageUrl !== null &&
+            this.props.info.price !== null &&
+            this.props.info.location !== "") {
+            this.setState({ isFulfilled: true })
         }
-      }
-    
-      componentWillUnmount(){
-          this.setState({isSubmitted: false});
-      }
-    render(){
+    }
+
+    submitImage() {
+        if (this.state.isFulfilled) {
+            axios.post('https://api.cloudinary.com/v1_1/daretodate/image/upload', {
+                "file": this.props.info.imagePreviewUrl,
+                "upload_preset": "v4gae7vn",
+            },
+                { "X-Requested-With": "XMLHttpRequest" })
+                .then(response => {
+                    this.setState({ image: response.data.secure_url })
+                    console.log(this.state.image)
+
+                    this.handleFormSubmit()
+                })
+        }
+        else {
+            alert("please fill all required fields.")
+        }
+    }
+
+
+    componentWillUnmount() {
+        this.setState({ isFulfilled: false });
+    }
+    render() {
 
         const { fireRedirect } = this.state
-        return(  
+        return (
             <div>
-            <Modal trigger={<Button disabled={false} style={{fontFamily: "'Open Sans', sans-serif"}}>Preview</Button>}>
-                <Modal.Header style={{color: "#5c7aff", fontFamily: "'Ubuntu', sans-serif"}}>Post Preview</Modal.Header>
-                <Modal.Content image scrolling>
-                <Image wrapped size='medium' src={this.props.info.imagePreviewUrl} rounded />
-                <Modal.Description>
-                    <p style={{fontFamily: "'Open Sans', sans-serif"}}>{this.props.info.location}</p>
-                    <p style={{fontFamily: "'Open Sans', sans-serif"}}>Title: {this.props.info.title}</p>
-                    <p style={{fontFamily: "'Open Sans', sans-serif"}}>Description: {this.props.info.description}</p>
-                </Modal.Description>
-                </Modal.Content>
-                <Modal.Actions>
-                <Button color="black" style={{fontFamily: "'Open Sans', sans-serif"}} onClick={this.submitImage.bind(this)}>
-                    Submit
+                <Modal trigger={<Button disabled={false} style={{ fontFamily: "'Open Sans', sans-serif" }}>Preview</Button>}>
+                    <Modal.Header style={{ color: "#5c7aff", fontFamily: "'Ubuntu', sans-serif" }}>Post Preview</Modal.Header>
+                    <Modal.Content image scrolling>
+                        <Image wrapped size='medium' src={this.props.info.imagePreviewUrl} rounded />
+                        <Modal.Description>
+                            <p style={{ fontFamily: "'Open Sans', sans-serif" }}>{this.props.info.location}</p>
+                            <p style={{ fontFamily: "'Open Sans', sans-serif" }}>Title: {this.props.info.title}</p>
+                            <p style={{ fontFamily: "'Open Sans', sans-serif" }}>Description: {this.props.info.description}</p>
+                        </Modal.Description>
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button color="black" style={
+                            this.state.isFulfilled ?
+                                { fontFamily: "'Open Sans', sans-serif" } :
+                                {
+                                    fontFamily: "'Open Sans', sans-serif",
+                                    cursor: "not-allowed"
+                                }
+                        }
+                            onClick={this.submitImage.bind(this)}>
+                            Submit
                 </Button>
-                </Modal.Actions>
-                {fireRedirect && (
-                    <Redirect to={'/'}/>
-                )}
-            </Modal>
+                    </Modal.Actions>
+                    {fireRedirect && (
+                        <Redirect to={'/'} />
+                    )}
+                </Modal>
 
             </div>
         );
